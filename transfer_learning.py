6341d49142189b4546b4d0e754a8328b18d2526e
floregol <florence.robert-regol@mail.mcgill.ca>
2023-06-15 13:51:05 -0400

    using the new checkpoint

--- transfer_learning.py
+++ transfer_learning.py
@@ -128,7 +128,7 @@ parser.add_argument(
     '--ckp-path',
     type=str,
     default=
-    'checkpoint/checkpoint_cifar10_t2t_vit_7/ckpt_0.05_0.0005_93.28.pth',
+    'checkpoint/checkpoint_cifar10_t2t_vit_7/ckpt_0.05_0.0005_90.47.pth',
     help='path to checkpoint transfer learning model')
 # us
 parser.add_argument('--use_mlflow',
@@ -250,10 +250,10 @@ if freeze_backbone:
     model_parameters = filter(lambda p: p.requires_grad, net.parameters())
     from_num_params = sum([np.prod(p.size()) for p in model_parameters])
     # set everything to not trainable.
-    for param in net.parameters():
+    for param in net.module.parameters():
         param.requires_grad = False
     # set the intermediate_heads params to trainable.
-    for param in net.intermediate_heads.parameters():
+    for param in net.module.intermediate_heads.parameters():
 
         param.requires_grad = True
     parameters = net.parameters()
