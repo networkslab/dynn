283f595c49f9588b1584972665115d02828f9ae7
floregol <florence.robert-regol@mail.mcgill.ca>
2023-07-04 17:44:15 -0400

    deeper network

--- src/models/custom_modules/learnable_code_gate.py
+++ src/models/custom_modules/learnable_code_gate.py
@@ -7,7 +7,7 @@ import numpy as np
 from sklearn import random_projection
 
 class LearnableCodeGate(Gate):
-    def __init__(self, device, input_dim, num_proj=4, proj_dim=16):
+    def __init__(self, device, input_dim, num_proj=4, proj_dim=32):
         super(Gate, self).__init__()
         self.gate_type = GateType.CODE
         self.num_proj =num_proj
@@ -19,12 +19,16 @@ class LearnableCodeGate(Gate):
             random_projs.append(torch.normal(0., 1/proj_dim, (self.input_dim, self.proj_dim)))
 
         self.random_projs = torch.cat(random_projs, dim=1).to(self.device)
-        self.linear = torch.nn.Linear(proj_dim*num_proj, 1)
+        self.linear1 = torch.nn.Linear(proj_dim*num_proj, proj_dim)
+        self.linear2 = torch.nn.Linear(proj_dim, 1)
 
     def forward(self, codes: Tensor) -> (Tensor):
         flat_code = codes.reshape(-1,self.input_dim )
         lower_dim_codes =  torch.matmul(flat_code.float(), self.random_projs)
-        return self.linear(lower_dim_codes)
+        x = self.linear1(lower_dim_codes)
+        x = torch.functional.F.relu(x)
+        x = self.linear2(x)
+        return x
 
     def inference_forward(self, input: Tensor, previous_mask: Tensor) -> Tuple[Tensor, Tensor]:
         pass
