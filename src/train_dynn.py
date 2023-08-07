b29c915204c2ca1020270b6509c487e9db5e49ec
floregol <florence.robert-regol@mail.mcgil.ca>
2023-08-07 10:47:30 -0400

    final push

--- src/train_dynn.py
+++ src/train_dynn.py
@@ -40,7 +40,7 @@ parser.add_argument('--bilevel_batch_count',default=200,type=int,help='number of
 parser.add_argument('--barely_train',action='store_true',help='not a real run')
 parser.add_argument('--resume','-r',action='store_true',help='resume from checkpoint')
 parser.add_argument('--model', type=str,default='learn_gate_direct')  # learn_gate, learn_gate_direct
-parser.add_argument('--gate',type=GateType,default=GateType.CODE_AND_UNC,choices=GateType)  # unc, code, code_and_unc
+parser.add_argument('--gate',type=GateType,default=GateType.UNCERTAINTY,choices=GateType)  # unc, code, code_and_unc
 parser.add_argument('--drop-path',type=float,default=0.1,metavar='PCT',help='Drop path rate (default: None)')
 parser.add_argument('--gate_selection_mode', type=GateSelectionMode, default=GateSelectionMode.DETERMINISTIC, choices=GateSelectionMode)
 parser.add_argument('--gate_objective', type=GateObjective, default=GateObjective.Prob, choices=GateObjective)
@@ -173,7 +173,7 @@ elif 'baseline' in args.arch: # only training with warmup
     
     for epoch in range(0, args.num_epoch):
         train_single_epoch(args, learning_helper, device, train_loader, epoch=epoch, training_phase=TrainingPhase.WARMUP, bilevel_batch_count=args.bilevel_batch_count, warmup_batch_count=args.warmup_batch_count)
-        stored_metrics_test = test(best_acc, args, learning_helper, device, test_loader, epoch, freeze_classifier_with_val=False)
+        #stored_metrics_test = test(best_acc, args, learning_helper, device, test_loader, epoch, freeze_classifier_with_val=False)
         fixed_threshold_test(args,learning_helper, device, test_loader, val_loader)
         scheduler.step()
 
