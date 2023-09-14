f9f60dfe53a4dd950d98f3e68fd899a4334d6ee4
floregol <florence.robert-regol@mail.mcgill.ca>
2023-09-13 20:35:40 -0400

    udpate the req

--- src/models/op_counter.py
+++ src/models/op_counter.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
-from src.models.custom_modules.learnable_uncertainty_gate import LearnableUncGate
+from models.custom_modules.learnable_uncertainty_gate import LearnableUncGate
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
